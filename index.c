72b9b498d596508b3fa0dc17124ebdce0e1991a8
naimur.a.rahman <naimur.a.rahman@doyenhub.com>
2022-04-16 22:38:08 +0600

    switch added

--- index.c
+++ index.c
@@ -2,6 +2,26 @@
 
 int main()
 {
-    int num;
-    scanf("%d ", &num);
+    //    Learning switch
+    int num1, num2;
+    int result;
+    char op;
+    scanf("%d %c %d", &num1, &op, &num2);
+    switch (op)
+    {
+
+    case '+':
+        printf("%d", num1 + num2);
+        break;
+    case '-':
+        printf("%d", num1 - num2);
+        break;
+    case '*':
+        printf("%d", num1 * num2);
+        break;
+    case '/':
+        printf("%d", num1 / num2);
+        break;
+    }
+    printf("%d", result);
 }
